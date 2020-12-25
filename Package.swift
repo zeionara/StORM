2ab1fdc03ed4f415e1f0164eafb3088f2bb3042c
Dmitrii <zeionara@gmail.com>
2020-12-25 18:12:54 +0300

    Fixed dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 // Generated automatically by Perfect Assistant Application
 // Date: 2018-03-02 16:12:45 +0000
 import PackageDescription
@@ -8,13 +8,14 @@ let package = Package(
 		.library(name: "StORM", targets: ["StORM"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "4.0.0"),
 		.package(url: "https://github.com/iamjono/SwiftMoment.git", from: "1.0.0")
 	],
 	targets: [
-		.target(name: "StORM", dependencies: [
-			"SwiftMoment",
-			"PerfectLib"
-		])
+				.target(name: "StORM", dependencies: [
+					"SwiftMoment",
+					"PerfectLib"
+			]
+		)
 	]
 )
